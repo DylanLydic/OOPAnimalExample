1dd2724753d9d590f6c1e49a08731cc6dc5f0082
DylanLydic <77704203+DylanLydic@users.noreply.github.com>
2021-02-02 20:23:40 -0500

    First Checkin

---
+++ OOPAnimal/OOPAnimal/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAnimal
+{
+    class Family
+    {
+        public string Name;
+        public int numberofToes;
+        public int numberofTeeth;
+        public bool whiskers;
+        public bool doesJeremyLikeIt;
+        public Family(string _name = null)
+        {
+            Name = _name;
+            Console.WriteLine(" Creating " + _name + " at " + DateTime.Now.ToString("yyyy-MM-dd"));
+         }
+
+        public void setAttributes(Family sentFamily)
+        {
+            numberofTeeth = sentFamily.numberofTeeth;
+            numberofToes = sentFamily.numberofToes;
+        }
+
+        public bool doesJermeyLikeThisFamily()
+        {
+            return doesJeremyLikeIt;
+        }
+
+    }
+}
---
+++ OOPAnimal/OOPAnimal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a tiger, and a caracel
+            Species tiger = new Species("Tiger", "Panthera tigris");
+            Species caracal = new Species("Caracal");
+            Family Felidae = new Family("Felidae");
+
+            Felidae.numberofToes = 5;
+            Felidae.numberofTeeth = 30;
+
+            tiger.setAttributes(Felidae);
+            caracal.setAttributes(Felidae);
+
+            Console.WriteLine("What is the name of the Species?:");
+            string newSpeciesName = Console.ReadLine();
+            
+            
+            Console.WriteLine("How many legs does it have?:");
+            int numLegs = 0;
+
+            try
+            {
+                 numLegs = int.Parse(Console.ReadLine());
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine("Couldn't make a number out of that, please try again" + ex.ToString());
+                while (numLegs <= 0)
+                {
+                    Console.WriteLine("How many legs does it have?:");
+                    try
+                    {
+                        numLegs = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex2) { }
+                }
+            }
+            
+
+            
+
+            Species newObj = new Species(newSpeciesName, numLegs);
+
+            Console.ReadLine();
+
+        }
+    }
+}
